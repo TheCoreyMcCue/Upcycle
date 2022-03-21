0d9eacdcf755554b3a6ab027c528b2e382172df0
Corey McCue <coreysmccue@gmail.com>
2022-03-21 13:53:10 +0100

    begin structure and style for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import './Header.css';
+import logo from "../../assets/logos/UpcycleSkate-logos_white.png";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <img className="header__logo" alt="Upcycle Skate logo" src={logo} />
+      <div className="header__search">
+        <input className="header__searchInput" type="text"></input>
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest </span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo"> & Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your </span>
+          <span className="header__optionLineOne">Dashboard</span>
+        </div>
+        <div className="header__option"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
