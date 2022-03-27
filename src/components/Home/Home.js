819fcc940a29c49fdeea9e2d8a2163fa79cdfe46
Corey McCue <coreysmccue@gmail.com>
2022-03-27 20:02:20 +0200

    add dependencies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,11 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home">
-      
+      <div className="home__container">
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
