aa229e1269f29ab096e662d1ee122fc3882e1baf
Corey McCue <coreysmccue@gmail.com>
2022-03-27 21:59:50 +0200

    add navbar instead of original header and banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,14 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home__container">
-
+        <img
+          className="home__image"
+          src="https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+          alt=""
+        />
       </div>
+
+      <div className="home__row"></div>
     </div>
   );
 }
