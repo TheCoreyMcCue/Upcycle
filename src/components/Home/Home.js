edf7ef30ec04f26f634d04f0880332dab1ab7028
Corey McCue <coreysmccue@gmail.com>
2022-03-28 22:32:45 +0200

    add seeded data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,44 +17,44 @@ const Home = () => {
       <div className="home__row">
         <Listing
           title="Complete Element Deck with Spitfire Wheels"
-          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          image="https://imgs.search.brave.com/e8XvhwcJ1pIxbd4wXj8HzEpjDsIHfCtB3ZgHQW3Hjl4/rs:fit:1000:1000:1/g:ce/aHR0cHM6Ly93d3cu/bmF0aXZlc2thdGVz/dG9yZS5jby51ay9p/bWFnZXMvZHllLXRy/aXBwaW4tY29tcGxl/dGUtc2thdGVib2Fy/ZC03LTc1LXAzNjc0/Ny05MTIxOF9pbWFn/ZS5qcGc"
           price="120"
           rating={4}
         />
         <Listing
-          title="Complete Element Deck with Spitfire Wheels"
-          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
-          price="120"
-          rating={4}
+          title="Complete Custom Burton Deck"
+          image="https://imgs.search.brave.com/bSVOiN3eN1b-3pWeyDa0jO-HMD0wfp6wOWXdytsnG7U/rs:fit:1200:757:1/g:ce/aHR0cHM6Ly93d3cu/cGV0ZXJ2ZXJkb25l/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAxNC8wOC9BTkRS/MDUyMC5qcGc"
+          price="85"
+          rating={5}
         />
       </div>
 
       <div className="home__row">
         <Listing
-          title="Complete Element Deck with Spitfire Wheels"
-          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
-          price="120"
-          rating={4}
-        />
-        <Listing
-          title="Complete Element Deck with Spitfire Wheels"
-          image="https://imgs.search.brave.com/vMVe7S1wzhqYOlazOXwj6X3LU3h_KSZrg58czco0G6U/rs:fit:1200:1152:1/g:ce/aHR0cHM6Ly9jb250/ZW50Lmluc3RydWN0/YWJsZXMuY29tL09S/SUcvRkVELzhZWVIv/SFpJOUJOVlkvRkVE/OFlZUkhaSTlCTlZZ/LmpwZz9mcmFtZT0x"
-          price="30"
+          title="Rocket Powered Custom Build"
+          image="https://imgs.search.brave.com/-x4cCGSOunjxt91o-cT4xPs_OFtCfbbC8jPHS7GhQXg/rs:fit:1200:908:1/g:ce/aHR0cHM6Ly9ldm9s/dmVmb3J1bXMuY29t/L2F0dGFjaG1lbnRz/L3VwbG9hZF8yMDE2/LTEyLTExXzAtMzIt/MzUtcG5nLjY2My8"
+          price="99"
           rating={3}
         />
+        <Listing
+          title="Street Deck Complete"
+          image="https://imgs.search.brave.com/vMVe7S1wzhqYOlazOXwj6X3LU3h_KSZrg58czco0G6U/rs:fit:1200:1152:1/g:ce/aHR0cHM6Ly9jb250/ZW50Lmluc3RydWN0/YWJsZXMuY29tL09S/SUcvRkVELzhZWVIv/SFpJOUJOVlkvRkVE/OFlZUkhaSTlCTlZZ/LmpwZz9mcmFtZT0x"
+          price="30"
+          rating={5}
+        />
       </div>
 
       <div className="home__row">
         <Listing
-          title="Complete Element Deck with Spitfire Wheels"
-          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
-          price="120"
+          title="Old School Cruiser"
+          image="https://imgs.search.brave.com/aEoDgPtZ-KnhNqSph3ijl7HhBEH9OpWuvkJJ6raqOYE/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly9ibG9n/LmFkYWZydWl0LmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/Ni8wOC9pbWdfOTE2/Mi5qcGc"
+          price="145"
           rating={5}
         />
         <Listing
-          title="Complete Element Deck with Spitfire Wheels"
-          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
-          price="120"
+          title="Custom Joe Mamma Deck"
+          image="https://imgs.search.brave.com/x9BEjMesrUCKMTk0_3LgwZD6fSKz7dc3GylnG49CBMQ/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/Ym9hcmRwdXNoZXIu/Y29tL2Jsb2cvd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMTIv/Ym9hcmRwdXNoZXIt/ZGlnaXRhbC1jYXJi/aW5lLWRlYWQtc2Ft/dXJhaS1za3VsbC1j/dXN0b20tc2thdGVi/b2FyZC1iaWcuanBn"
+          price="80"
           rating={4}
         />
       </div>
--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -5,16 +5,21 @@ const listing = ({ title, image, price, rating}) => {
   return (
     <div className="listing">
       <div className="listing__info">
-        <h6>{title}</h6>
+        <h5>{title}</h5>
         <div className="listing__price">
           <p>
             <small>$</small>
             <strong>{price}</strong>
           </p>
+          <p>
+            <small>seller rating: </small>
+          </p>
           <div className="listing__rating">
-            {Array(rating).map((rating) => (
-              <p>️️⭐</p>
-            ))}
+            {Array(rating)
+              .fill()
+              .map((rating) => (
+                <p>️️⭐</p>
+              ))}
           </div>
         </div>
       </div>
