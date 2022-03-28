d0e8c83a911508d1cdf09fb763155c0e04a97663
Corey McCue <coreysmccue@gmail.com>
2022-03-28 22:12:16 +0200

    add props to listing cmponent

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,20 +14,50 @@ const Home = () => {
         />
       </div>
       {/* <div className="listings__container"> */}
-        <div className="home__row">
-          <Listing />
-          <Listing />
-        </div>
+      <div className="home__row">
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          price="120"
+          rating={4}
+        />
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          price="120"
+          rating={4}
+        />
+      </div>
 
-        <div className="home__row">
-          <Listing />
-          <Listing />
-        </div>
+      <div className="home__row">
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          price="120"
+          rating={4}
+        />
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://imgs.search.brave.com/vMVe7S1wzhqYOlazOXwj6X3LU3h_KSZrg58czco0G6U/rs:fit:1200:1152:1/g:ce/aHR0cHM6Ly9jb250/ZW50Lmluc3RydWN0/YWJsZXMuY29tL09S/SUcvRkVELzhZWVIv/SFpJOUJOVlkvRkVE/OFlZUkhaSTlCTlZZ/LmpwZz9mcmFtZT0x"
+          price="30"
+          rating={3}
+        />
+      </div>
 
-        <div className="home__row">
-          <Listing />
-          <Listing />
-        </div>
+      <div className="home__row">
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          price="120"
+          rating={5}
+        />
+        <Listing
+          title="Complete Element Deck with Spitfire Wheels"
+          image="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
+          price="120"
+          rating={4}
+        />
+      </div>
       {/* </div> */}
     </div>
   );
--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,30 +1,24 @@
 import React from "react";
 import "./Listing.css";
-import { Button } from "@material-ui/core";
 
-const listing = () => {
+const listing = ({ title, image, price, rating}) => {
   return (
     <div className="listing">
       <div className="listing__info">
-        <h6>Complete Element Deck with Spitfire Wheels</h6>
+        <h6>{title}</h6>
         <div className="listing__price">
           <p>
             <small>$</small>
-            <strong>120</strong>
+            <strong>{price}</strong>
           </p>
           <div className="listing__rating">
-            <p>️️⭐</p>
-            <p>️️⭐</p>
-            <p>️️⭐</p>
-            <p>️️⭐</p>
+            {Array(rating).map((rating) => (
+              <p>️️⭐</p>
+            ))}
           </div>
         </div>
       </div>
-      <img
-        className="listing__image"
-        src="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3403&q=80"
-        alt=""
-      />
+      <img className="listing__image" src={image} alt="" />
       <button className="listing__button">Add to Cart</button>
     </div>
   );
