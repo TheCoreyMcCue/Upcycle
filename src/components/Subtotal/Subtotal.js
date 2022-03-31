18549fb578784a9c13c800313d8763949dce2dd1
Corey McCue <coreysmccue@gmail.com>
2022-03-31 21:44:41 +0200

    add currency format dependency

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -1,9 +1,27 @@
 import React from "react";
+import CurrencyFormat from "react-currency-format";
+import "./Subtotal.css";
 
 const Subtotal = () => {
   return (
-    <div>
-      <h4>Your Subtotal is: </h4>
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({0} items): <strong>{`${0}`}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
     </div>
   );
 };
